ef42b9ec021a97b0615202b45a4f771d49a3bb26
Rajat Jindal <rajatjindal83@gmail.com>
2019-11-22 18:13:51 +0530

    add license

--- build.sh
+++ build.sh
@@ -11,12 +11,14 @@ rm -rf _dist || true
 mkdir -p _dist
 # env GOOS=windows GOARCH=amd64 go build -ldflags "-X github.com/rajatjindal/kubectl-evict-pod/cmd.Version=$version" -o _dist/kubectl-evict-pod-windows-amd64-$version.exe main.go
 env GOOS=darwin  GOARCH=amd64 go build -ldflags "-w -s -X github.com/rajatjindal/kubectl-evict-pod/pkg/cmd.Version=$version" -o _dist/darwin-amd64/kubectl-evict-pod      main.go
-cd _dist/darwin-amd64/ && tar -cvzf darwin-amd64-$version.tar.gz kubectl-evict-pod
+cp LICENSE _dist/darwin-amd64/
+cd _dist/darwin-amd64/ && tar -cvzf darwin-amd64-$version.tar.gz kubectl-evict-pod LICENSE
 shasum -a 256 darwin-amd64-$version.tar.gz | awk '{print $1}' > sha256-darwin-amd64-$version
 cd -
 
 env GOOS=linux   GOARCH=amd64 go build -ldflags "-w -s -X github.com/rajatjindal/kubectl-evict-pod/pkg/cmd.Version=$version" -o _dist/linux-amd64/kubectl-evict-pod       main.go
-cd _dist/linux-amd64/  && tar -cvzf linux-amd64-$version.tar.gz kubectl-evict-pod
+cp LICENSE _dist/linux-amd64/
+cd _dist/linux-amd64/  && tar -cvzf linux-amd64-$version.tar.gz kubectl-evict-pod LICENSE
 shasum -a 256 linux-amd64-$version.tar.gz | awk '{print $1}' > sha256-linux-amd64-$version
 
 cd -
