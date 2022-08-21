825da70fd15cf783b96fe05aaec975c2584d3815
Rajat Jindal <rajatjindal83@gmail.com>
2022-08-21 09:29:36 +0530

    fix params

--- pkg/k8s/evict.go
+++ pkg/k8s/evict.go
@@ -1,6 +1,7 @@
 package k8s
 
 import (
+	"context"
 	policy "k8s.io/api/policy/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
@@ -8,7 +9,7 @@ import (
 
 //Evict try to evicts a pod
 func Evict(kubeclient kubernetes.Interface, podname, namespace string) error {
-	return kubeclient.PolicyV1beta1().Evictions(namespace).Evict(&policy.Eviction{
+	return kubeclient.PolicyV1beta1().Evictions(namespace).Evict(context.TODO(), &policy.Eviction{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      podname,
 			Namespace: namespace,
